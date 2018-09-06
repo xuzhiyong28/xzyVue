8f8e30034255632ef8cf9dcd51ef60f0c222fed4
xuzhiyong28 <jip0574@qq.com>
2018-09-06 18:31:53 +0800

    my first vue

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,12 +1,8 @@
 const Mock = require('mockjs');
 const Random = Mock.Random;
 // mock一组数据
-const produceNewsData = function () {
-  let articles = [
-    {'id' : '1' , 'title' : '许志勇'}
-  ];
-  return {
-    articles : articles
-  }
+const getNewIndexData = function () {
+  let json = require('./new-index.json');
+  return json;
 }
-Mock.mock('/news/index', /post|get/i, produceNewsData);
+Mock.mock('/news/index', /post|get/i, getNewIndexData);
