bc1f20f479720c18e95f997dc87d8590dec6e5f1
xuzhiyong28 <jip0574@qq.com>
2019-03-08 10:07:48 +0800

    update to github

--- src/router/message.js
+++ src/router/message.js
@@ -4,8 +4,16 @@ import Router from './index'
  * 路由拦截 - 全局前置守卫
  */
 Router.beforeEach((to , from , next) => {
-  console.log('to=' + JSON.stringify(to));
-  console.log('from=' + JSON.stringify(from));
+  console.log('before_to=' + JSON.stringify(to));
+  console.log('before_from=' + JSON.stringify(from));
   //next() - 下一步  next(false) --中断  next("/xxx")--跳转到另外一个视图
   next();
+})
+
+/****
+ * 路由拦截 - 全局后置守卫
+ */
+Router.afterEach((to,from) => {
+  console.log('after_to=' + JSON.stringify(to));
+  console.log('after_from=' + JSON.stringify(from));
 })
