ee4fb781c3b219c3b8314fe10da46b59bc6520ee
xuzhiyong28 <jip0574@qq.com>
2019-04-15 09:33:20 +0800

    update to github

--- src/components/ChildrenCompents.vue
+++ src/components/ChildrenCompents.vue
@@ -1,11 +1,70 @@
+<!--生命周期函数-->
 <template>
-    <div>我是通过连接展示的子节点</div>
+  <div>我是通过连接展示的子节点</div>
 </template>
 
 <script>
-    export default {
-        name: "children-compents"
+  export default {
+    name: "children-compents",
+    data() {
+      return {}
+    },
+    beforeCreate: function () {
+      console.group('beforeCreate 创建前状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+      console.log("%c%s", "color:red", "data   : " + this.$data); //undefined
+      console.log("%c%s", "color:red", "message: " + this.message)
+    },
+    created: function () {
+      console.group('created 创建完毕状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+      console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+      console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+    },
+    beforeMount: function () {
+      console.group('beforeMount 挂载前状态===============》');
+      console.log("%c%s", "color:red", "el     : " + (this.$el)); //已被初始化
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+      console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+    },
+    mounted: function () {
+      console.group('mounted 挂载结束状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el); //已被初始化
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+      console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+    },
+    beforeUpdate: function () {
+      console.group('beforeUpdate 更新前状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el);
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data);
+      console.log("%c%s", "color:red", "message: " + this.message);
+    },
+    updated: function () {
+      console.group('updated 更新完成状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el);
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data);
+      console.log("%c%s", "color:red", "message: " + this.message);
+    },
+    beforeDestroy: function () {
+      console.group('beforeDestroy 销毁前状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el);
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data);
+      console.log("%c%s", "color:red", "message: " + this.message);
+    },
+    destroyed: function () {
+      console.group('destroyed 销毁完成状态===============》');
+      console.log("%c%s", "color:red", "el     : " + this.$el);
+      console.log(this.$el);
+      console.log("%c%s", "color:red", "data   : " + this.$data);
+      console.log("%c%s", "color:red", "message: " + this.message)
     }
+
+  }
 </script>
 
 <style scoped>
--- src/router/message.js
+++ src/router/message.js
@@ -4,8 +4,8 @@ import Router from './index'
  * 路由拦截 - 全局前置守卫
  */
 Router.beforeEach((to , from , next) => {
-  console.log('before_to=' + JSON.stringify(to));
-  console.log('before_from=' + JSON.stringify(from));
+  console.log('before_to=' + to);
+  console.log('before_from=' + from);
   //next() - 下一步  next(false) --中断  next("/xxx")--跳转到另外一个视图
   /*if(to.matched.some(current => current.meta.needLogin)){
     console.log("===这个地址需要校验是否登录===");
@@ -17,6 +17,6 @@ Router.beforeEach((to , from , next) => {
  * 路由拦截 - 全局后置守卫
  */
 Router.afterEach((to,from) => {
-  console.log('after_to=' + JSON.stringify(to));
-  console.log('after_from=' + JSON.stringify(from));
+  console.log('after_to=' + to);
+  console.log('after_from=' + from);
 })
